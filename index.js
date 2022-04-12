9e1833683cd2b017d2331c935cc5267d8cae7370
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-04-12 15:40:46 -0700

    Initial addition of working code

---
+++ index.js
@@ -0,0 +1,86 @@
+class AlphanumericEncoder {
+    constructor() {
+        this._dictionary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'     //Default dictionary is the English alphabet, all capitalized, in order
+    }
+
+    get dictionary() {
+        return this._dictionary
+    }
+    set dictionary(newDictionary) {
+        //Check for empty dictionaries
+        if (newDictionary === null || newDictionary === undefined || newDictionary.length === 0) {
+            throw new Error('The dictionary cannot be null, undefined, or an empty string.')
+        }
+
+        //Check for invalid characters. Using a regular expression, make sure only letters and numbers are allowed.
+        const regExPattern = /^[a-z0-9]+$/i
+        if (!regExPattern.test(newDictionary)) {
+            throw new Error('All characters in the dictionary must be alphanumeric.')
+        }
+
+        //Convert to upper case only. Verify each character is only used one time within the dictionary.
+        newDictionary = newDictionary.toUpperCase()
+        for (let i = 0; i < newDictionary.length; i++) {
+            if(newDictionary.indexOf(newDictionary[i]) !== newDictionary.lastIndexOf(newDictionary[i])) {
+                throw new Error(`The dictionary in use has at least one repeating symbol: ${newDictionary[i]}`)
+            }
+        }
+
+        //Validation is complete. Update the internal property.
+        this._dictionary = newDictionary
+    }
+
+    encode(number) {
+        //Takes any number and converts it into a base (dictionary length) letter combo.
+        //It converts any numerical entry into a positive integer.
+        if (isNaN(number) || number < 0) {return undefined}         //!Number.isInteger(number)
+
+        number = Math.abs(Math.floor(number))
+
+        let index = number % this.dictionary.length
+        let quotient = number / this.dictionary.length
+        let result
+        
+        if (number <= this.dictionary.length) {
+            //Number is within single digit bounds of our encoding letter alphabet
+            return numToLetter(number, this.dictionary)
+        }
+
+        //By reaching this point in the code, this number was bigger than our dictionary. Recursively perform this function until complete
+        if (index === 0) {quotient--}   //Accounts for the edge case of the last letter in the dictionary string
+        result = this.encode(quotient)
+        
+        if (index === 0) {index = this.dictionary.length}   //Accounts for the edge case of the final letter; avoids getting an empty string
+        
+        return result + numToLetter(index, this.dictionary)
+
+        function numToLetter(number, dictionary) {
+            //Takes a letter between 0 and max letter length and returns the corresponding letter
+            if (number === 0) {
+                return undefined
+            } else {
+                return dictionary.slice(number - 1, number)
+            }
+        }
+    }
+
+    decode(encoded) {
+        //Takes any number encoded with the provided encode dictionary 
+
+        encoded = encoded + ''  //Force any numbers passed into a string format to get proper behavior below
+        if (encoded === '' || encoded === null || encoded === undefined) {return undefined}
+
+        let result = 0
+        let index = 0
+
+        for (let i = 1; i <= encoded.length; i++) {
+            index = this.dictionary.search(encoded.slice(i - 1, i)) + 1
+            if (index === 0) {return undefined} //Attempted to find a letter that wasn't encoded in the dictionary
+            result = result + index * Math.pow(this.dictionary.length, (encoded.length - i))
+        }
+
+        return result
+    }
+}
+
+module.exports = AlphanumericEncoder
---
+++ index.test.js
@@ -0,0 +1,190 @@
+let AlphanumericEncoder = require('./index')
+
+let encoder = new AlphanumericEncoder
+
+const numberToEncodedLetters = [
+    [1, 'A'],
+    [4, 'D'],
+    [25, 'Y'],
+    [26, 'Z'],
+    [27, 'AA'],
+    [52, 'AZ'],
+    [53, 'BA'],
+    [78, 'BZ'],
+    [705, 'AAC']
+]
+
+const numberWithDictionaryToEncodedLetters = [
+    ['ABC', 4, 'AA'],
+    ['123ABC', 4, 'A'],
+    ['123ABC', 2, '2'],
+    ['123ABC', 200, 'B32'],
+    ['0123456789ABCDEF', 1, '0'],
+    ['0123456789ABCDEF', 16, 'F'],
+    ['0123456789ABCDEF', 17, '00'],
+    ['0123456789ABCDEF', 272, 'FF'],
+    ['EDCBA', 4, 'B'],
+    ['EDCBA', 7, 'ED'],
+    ['EDCBA', 26, 'AE'],
+    ['EDCBA', 27, 'AD'],
+    ['EDCBA', 31, 'EEE']
+]
+
+const expectedValidDictionaryValues = [
+    'ABCD',
+    'ABcd',
+    'aBcD',
+    'ABC123'
+]
+
+const expectedInvalidDictionaryValuesWithRepeatingSymbols = [
+    'ABCDA',
+    'ABCDa',
+    'ABCD1231'
+]
+
+const expectedInvalidDictionaryValuesWithImproperSymbols = [
+    'ABC!',
+    'ABC@',
+    'ABC?',
+    '&#$%(*)'
+]
+
+function setupNewEncoderForTesting() {
+    beforeEach(() => {
+        encoder = new AlphanumericEncoder
+    })
+
+    afterEach(() => {
+        encoder = null
+    })
+}
+
+describe('Dictionary Validation', () => {
+
+    setupNewEncoderForTesting()
+    test('Default dictionary should be capital alphabet in order', () => {
+        expect(encoder.dictionary).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    })
+
+    test('Dictionary cannot be an empty string', () => {
+        expect(() => {
+            encoder.dictionary = ''
+        }).toThrow(/empty string/)
+    })
+
+    test('Dictionary cannot be null', () => {
+        expect(() => {
+            encoder.dictionary = null
+        }).toThrow(/null/)
+    })
+
+    test('Dictionary cannot be undefined', () => {
+        expect(() => {
+            encoder.dictionary = undefined
+        }).toThrow(/undefined/)
+    })
+
+    describe('Valid Dictionaries', () => {
+        setupNewEncoderForTesting()
+
+        test.each(expectedValidDictionaryValues)('Expect %p to be a valid dictionary', (validDictionaryString) => {
+            encoder.dictionary = validDictionaryString
+            expect(encoder.dictionary).toBe(validDictionaryString.toUpperCase())
+        })
+
+        test('Dictionaries should be converted to uppercase', () => {
+            encoder.dictionary = 'abcd'
+            expect(encoder.dictionary).toBe('ABCD')
+        })
+    })
+
+    describe('Invalid Dictionaries', () => {
+        setupNewEncoderForTesting()
+
+        test.each(expectedInvalidDictionaryValuesWithRepeatingSymbols)('Expect %p to be a be invalid dictionary and throw error due to repeating symbol', (invalidDictionaryString) => {
+            
+            expect(() => {
+                encoder.dictionary = invalidDictionaryString
+            }).toThrow(/repeating symbol/)
+        })
+
+        test.each(expectedInvalidDictionaryValuesWithImproperSymbols)('Expect %p to be a be invalid dictionary due to non-alphanumeric symbol', (invalidDictionaryString) => {
+            
+            expect(() => {
+                encoder.dictionary = invalidDictionaryString
+            }).toThrow(/must be alphanumeric/)
+        })
+    })
+
+})
+
+describe('Test Encoding', () => {
+    setupNewEncoderForTesting()
+    
+    test('Expect 0 to return undefined', () => {
+        expect(encoder.encode(0)).toBeUndefined()
+    })
+
+    test('Expect negative numbers to return undefined', () => {
+        expect(encoder.encode(-1)).toBeUndefined()
+    })
+
+    test('Expect non-integers to return value of floor: e.g. 1.5 = "B", 3.1 = "C", 4.9 = "D"', () => {
+        expect(encoder.encode(2.5)).toBe('B')
+        expect(encoder.encode(3.1)).toBe('C')
+        expect(encoder.encode(4.9)).toBe('D')
+    })
+
+    describe('Encode with Default Dictionary', () => {
+        test.each(numberToEncodedLetters)('Under default dictionary, expect %p to encode to %p', (number, letter) => {
+            expect(encoder.encode(number)).toBe(letter)
+        })
+    })
+
+    describe('Encode with Custom Dictionary', () => {
+        test.each(numberWithDictionaryToEncodedLetters)('Under dictionary %p, expect %p to encode to %p', (dictionary, number, letter) => {
+            encoder.dictionary = dictionary
+            expect(encoder.encode(number)).toBe(letter)
+        })
+    })  
+})
+
+describe('Test Decoding', () => {
+    setupNewEncoderForTesting()
+
+    test.each(['', undefined, null])('Trying to decode %p should return "undefined"', (letter) => {
+        expect(encoder.decode(letter)).toBeUndefined()
+    })
+
+    describe('Invalid Decode Values Using Default Dictionary', () => {
+
+        const encodedWithNonexistentCharacter = [
+            0,
+            2,
+            -15,
+            -1.7,
+            22.6,
+            '16',
+            'D1',
+            'A#',
+            '&'
+        ]
+        test.each(encodedWithNonexistentCharacter)('Under default dictionary, trying to decode %p will return "undefined" due to character not existing in dictionary', (letter) => {
+            expect(encoder.decode(letter)).toBeUndefined()
+        })
+    })
+
+    describe('Decode with Default Dictionary', () => {
+        test.each(numberToEncodedLetters)('Under default dictionary, expect %p to be decoded from %p', (number, letter) => {
+            expect(encoder.decode(letter)).toBe(number)
+        })
+    })  
+
+    describe('Decode with Custom Dictionary', () => {
+        test.each(numberWithDictionaryToEncodedLetters)('Under dictionary %p, expect %p to be decoded from %p', (dictionary, number, letter) => {
+            encoder.dictionary = dictionary
+            expect(encoder.decode(letter)).toBe(number)
+        })
+    })  
+})
